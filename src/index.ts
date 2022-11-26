08f554bddb7a810086446033934fdf959441e423
DrumskY <bartekholikti1@gmail.com>
2022-11-26 17:14:47 +0100

    backend second commit

--- src/index.ts
+++ src/index.ts
@@ -13,11 +13,33 @@ app.use(cors());
 app.use(express.static("public"));
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello <3");
+  res.send("Hello Stranger <br /> https://youtu.be/dQw4w9WgXcQ");
 });
 
 app.get("/movies", async (req: Request, res: Response) => {
   const movies = await prisma.movie.findMany();
+  console.log(movies);
+  res.json(movies);
+});
+
+app.get("/movies/rating", async (req: Request, res: Response) => {
+  const movies = await prisma.movie.findMany({
+    orderBy: {
+      rating: "desc",
+    },
+  });
+  console.log(movies);
+  res.json(movies);
+});
+
+app.get("/movies/random", async (req: Request, res: Response) => {
+  const randomId = Math.floor(Math.random() * 10) + 1;
+  console.log(randomId);
+  const movies = await prisma.movie.findMany({
+    where: {
+      movieId: randomId,
+    },
+  });
   console.log(movies);
   res.json(movies);
 });
